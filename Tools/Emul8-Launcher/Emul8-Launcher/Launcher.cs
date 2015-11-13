b65b040457b6194dc4a9e00d988ecccbc197dd0a
Mateusz Hołenko <mholenko@antmicro.com>
2015-11-13 08:39:53 +0100

    [#8] Make `Emul8-Launcher` automatically detect binaries to launch.
    This commit introduces a mechanism of scanning `root-folder`
    for targets to launch. Only binaries that contain custom assembly
    attributes from `LaunchAttributes` project are detected.

--- Tools/Emul8-Launcher/Emul8-Launcher/Launcher.cs
+++ Tools/Emul8-Launcher/Emul8-Launcher/Launcher.cs
@@ -5,10 +5,11 @@
 // This file is part of the Emul8 project.
 // Full license details are defined in the 'LICENSE' file.
 //
-﻿using System;
-using System.IO;
+using System;
 using System.Diagnostics;
 using Antmicro.OptionsParser;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Emul8.Launcher
 {
@@ -16,57 +17,121 @@ namespace Emul8.Launcher
     {
         public static void Main(string[] args)
         {
-            var parser = new OptionsParser(new ParserConfiguration 
-            { 
-                GenerateHelp = true,
-                CustomFooterGenerator = () => "All unparsed options will be forwarded to CLI application.",
-                CustomUsageLineGenerator = usageLine => usageLine + " [script]",
+            var basicOptions = new BasicOptions();
+            var optionsParser = new OptionsParser(new ParserConfiguration
+            {
+                GenerateHelp = false,
                 AllowUnexpectedArguments = true
             });
-            
-            var options = new Options();
-            if(!parser.Parse(options, args))
+
+            // here we parse only one option - `root-directory`
+            // the rest will be parsed in the second phase
+            if(!optionsParser.Parse(basicOptions, args))
             {
                 return;
             }
-            
-            var mode = options.Debug ? "Debug" : "Release";
+
+            optionsParser = new OptionsParser(new ParserConfiguration
+            {
+                GenerateHelp = true,
+                CustomFooterGenerator = () => "All unparsed options will be forwarded to launched application.",
+                CustomUsageLineGenerator = usageLine => usageLine + " [script]",
+                AllowUnexpectedArguments = true
+            });
+
+            var addHelpSwitch = false;
+            var possibleLaunchees = new List<LaunchDescriptorsGroup>();
+            var selectedLaunchees = new List<LaunchDescriptorsGroup>();
+            foreach(var launchDescriptor in Scanner.ScanForInterestingBinaries(basicOptions.RootPath))
+            {
+                possibleLaunchees.Add(launchDescriptor);
+                launchDescriptor.GenerateSwitches(optionsParser);
+                launchDescriptor.SwitchOption.Parsed += (option, value) =>
+                {
+                    selectedLaunchees.Add(launchDescriptor);
+                };
+
+                if(launchDescriptor.HelpOption != null)
+                {
+                    launchDescriptor.HelpOption.Parsed += (option, value) =>
+                    {
+                        addHelpSwitch = true;
+                    };
+                }
+            }
+
+            // here we parse all of the options again
+            // as only now we know which launch targets
+            // and configurations are available in the
+            // system
+            var options = new Options();
+            if(!optionsParser.Parse(options, args))
+            {
+                return;
+            }
+
+            var conf = options.Debug ? LaunchDescriptor.Configuration.Debug : LaunchDescriptor.Configuration.Release;
+            LaunchDescriptorsGroup selectedGroup = null;
+            LaunchDescriptor selectedLaunchee = null;
+            if(selectedLaunchees.Count == 0)
+            {
+                if(possibleLaunchees.Count == 0)
+                {
+                    Console.Error.WriteLine("There is no launch target specified. Exiting");
+                    Environment.Exit(1);
+                }
+
+                selectedGroup = possibleLaunchees.OrderBy(x => x.Priority).ThenBy(x => x.Name).First();
+            }
+            else if(selectedLaunchees.Count > 1)
+            {
+                Console.Error.WriteLine("Only one launch target can be used. Exiting");
+                Environment.Exit(1);
+            }
+            else
+            {
+                selectedGroup = selectedLaunchees.First();
+            }
+
+            selectedLaunchee = selectedGroup.ForConfiguration(conf);
+            if(selectedLaunchee == null)
+            {
+                Console.Error.WriteLine("Selected target {0} is not available in {1} configuration.", selectedGroup.Name, conf);
+                Environment.Exit(1);
+            }
+
             var monoOptions = options.Debug ? "--debug" : string.Empty;
-            
+
             if(options.DebuggerSocketPort != -1)
             {
                 monoOptions += string.Format(" --debugger-agent=transport=dt_socket,address=127.0.0.1:{0},server=y", options.DebuggerSocketPort);
                 Console.WriteLine("Listening on port {0}", options.DebuggerSocketPort);
             }
-            
-            var rootPath = (options.RootPath == ".") ? Environment.CurrentDirectory : options.RootPath;
-            Console.WriteLine("Running in {0} mode.\nROOT_PATH={1}", mode, rootPath);
-            
-            var binaryPath = Path.Combine(rootPath, "output", mode, "CLI.exe");
-            var optionsToPass = parser.RecreateUnparsedArguments();
-            if(options.HelpCLI)
+
+            var optionsToPass = optionsParser.RecreateUnparsedArguments();
+            if(addHelpSwitch)
             {
-                Console.WriteLine("NOTE: showing help from binary: {0}", binaryPath);
+                Console.WriteLine("NOTE: showing help from binary: {0}", selectedLaunchee.Name);
                 optionsToPass = "--help";
             }
-            
+
             var process = new Process();
             process.StartInfo.FileName = "mono";
-            process.StartInfo.Arguments = string.Format("{0} {1} {2}", monoOptions, binaryPath, optionsToPass);
-            
+            process.StartInfo.Arguments = string.Format("{0} {1} {2}", monoOptions, selectedLaunchee.Path, optionsToPass);
+
             if(options.Quiet)
             {
                 process.StartInfo.RedirectStandardError = true;
                 process.StartInfo.UseShellExecute = false;
             }
-            
+
             process.Start();
             if(options.Quiet)
             {
                 process.StandardError.Close();
             }
-            
+
             process.WaitForExit();
-        } 
+        }
     }
 }
