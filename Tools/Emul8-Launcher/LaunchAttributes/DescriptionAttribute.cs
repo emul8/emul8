b8318d5264b4edfb7f13abdd61cdd44980492eb3
Mateusz Hołenko <mholenko@antmicro.com>
2015-11-13 08:28:38 +0100

    [#8] Add `LaunchAttributes` project.
    This commit introduces a project with custom assembly
    attributes that will be used to annotate binaries
    detectable by launcher.

---
+++ Tools/Emul8-Launcher/LaunchAttributes/DescriptionAttribute.cs
@@ -0,0 +1,21 @@
+﻿//
+// Copyright (c) Antmicro
+// Copyright (c) Realtime Embedded
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+using System;
+
+namespace Emul8.LaunchAttributes
+{
+    public class DescriptionAttribute : Attribute
+    {
+        public DescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+
+        public string Description { get; private set; }
+    }
+}
---
+++ Tools/Emul8-Launcher/LaunchAttributes/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿//
+// Copyright (c) Antmicro
+// Copyright (c) Realtime Embedded
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Emul8 launch attributes")]
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Antmicro, Realtime Embedded")]
+[assembly: AssemblyProduct("Emul8")]
+[assembly: AssemblyCopyright("(c) Antmicro, Realtime Embedded")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
