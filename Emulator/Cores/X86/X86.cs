e5e58b16b3a87bf2b539430b567feefdaf3f44c7
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2015-12-17 15:45:12 +0100

    [FIX] Readd I/O port API to moved X86.cs.

--- Emulator/Cores/X86/X86.cs
+++ Emulator/Cores/X86/X86.cs
@@ -6,9 +6,7 @@
 // Full license details are defined in the 'LICENSE' file.
 //
 using Emul8.Core;
-using Emul8.Time;
-using System.Collections.Generic;
-using System;
+using Emul8.Utilities.Binding;
 
 namespace Emul8.Peripherals.CPU
 {
@@ -28,6 +26,45 @@ namespace Emul8.Peripherals.CPU
             }
             throw InvalidInterruptNumberException;
         }
+
+        [Export]
+        private uint ReadByteFromPort(uint address)
+        {
+            return ReadByteFromBus(IoPortBaseAddress + address);
+        }
+
+        [Export]
+        private uint ReadWordFromPort(uint address)
+        {
+            return ReadWordFromBus(IoPortBaseAddress + address);
+        }
+
+        [Export]
+        private uint ReadDoubleWordFromPort(uint address)
+        {
+            return ReadDoubleWordFromBus(IoPortBaseAddress + address);
+        }
+
+        [Export]
+        private void WriteByteToPort(uint address, uint value)
+        {
+            WriteByteToBus(IoPortBaseAddress + address, value);
+
+        }
+
+        [Export]
+        private void WriteWordToPort(uint address, uint value)
+        {
+            WriteWordToBus(IoPortBaseAddress + address, value);
+        }
+
+        [Export]
+        private void WriteDoubleWordToPort(uint address, uint value)
+        {
+            WriteDoubleWordToBus(IoPortBaseAddress + address, value);
+        }
+
+        private const uint IoPortBaseAddress = 0xE0000000;
     }
 }
 
