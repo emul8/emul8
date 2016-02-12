56751ab78ffa4a686fd2554bc793a7034f026a02
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2016-02-12 15:30:58 +0100

    [#71] Watchpoint infrastructure.

---
+++ Emulator/Main/Peripherals/Bus/Width.cs
@@ -0,0 +1,20 @@
+﻿//
+// Copyright (c) Antmicro
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+using System;
+
+namespace Emul8.Peripherals.Bus
+{
+    [Flags]
+    public enum Width
+    {
+        Byte = 1,
+        Word = 2,
+        DoubleWord = 4,
+        QuadWord = 8
+    }
+}
+
