8f900cc2a352bc54922da20d4ec54044edc13093
Konrad M. Kruczyński <kkruczynski@antmicro.com>
2016-05-09 14:51:01 +0200

    [#6100] Added test for synced clock sources.
    Also fixed an old test.

--- Emulator/Main/Tests/UnitTests/TimeTests.cs
+++ Emulator/Main/Tests/UnitTests/TimeTests.cs
@@ -8,6 +8,7 @@
 using System;
 using NUnit.Framework;
 using Emul8.Time;
+using System.Collections.Generic;
 
 namespace UnitTests
 {
@@ -50,6 +51,26 @@ namespace UnitTests
 			Assert.AreEqual(2, counterA);
 			Assert.AreEqual(1, counterB);
 		}
+
+        [Test]
+        public void ShouldHaveHandlersInSync()
+        {
+            var clockSource = new BaseClockSource();
+
+            Action firstHandler = () =>
+            {
+            };
+
+            var values = new List<long>();
+            clockSource.AddClockEntry(new ClockEntry(10000, 1, firstHandler));
+            clockSource.AddClockEntry(new ClockEntry(1, -10, () => values.Add(clockSource.GetClockEntry(firstHandler).Value)));
+
+            clockSource.Advance(9, true);
+            clockSource.Advance(8, true);
+            clockSource.Advance(20, true);
+
+            CollectionAssert.AreEqual(new [] { 10, 20, 30 }, values);
+        }
 	}
 }
 
