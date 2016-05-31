2c5317818b27c46ce3de7ffdb4e79a30d5101178
Konrad M. Kruczyński <kkruczynski@antmicro.com>
2016-05-31 16:00:41 +0200

    [#6007] Let interface files given without path have default one.

--- Emulator/Extensions/HostInterfaces/Network/TapExtensions.cs
+++ Emulator/Extensions/HostInterfaces/Network/TapExtensions.cs
@@ -10,6 +10,7 @@ using Emul8.Core;
 using Emul8.Peripherals.Network;
 using Emul8.Utilities;
 using Emul8.Exceptions;
+using System.IO;
 
 namespace Emul8.HostInterfaces.Network
 {
@@ -17,12 +18,25 @@ namespace Emul8.HostInterfaces.Network
     {
         public static IMACInterface CreateAndGetTap(this Emulation emulation, string hostInterfaceName, string name, bool persistent = false)
         {
+            ITapInterface result;
             var onOsX = Misc.IsOnOsX;
-            if(onOsX && persistent)
+            if(onOsX)
             {
-                throw new RecoverableException("Persitent TAP is not available on OS X.");
+                if(persistent)
+                {
+                    throw new RecoverableException("Persitent TAP is not available on OS X.");
+                }
+                if(!File.Exists(hostInterfaceName))
+                {
+                    var tapDevicePath = ConfigurationManager.Instance.Get<string>("tap", "tap-device-path", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+                    hostInterfaceName = Path.Combine(tapDevicePath, hostInterfaceName);
+                }
+                result = new OsXTapInterface(hostInterfaceName);
             }
-            var result = onOsX ? (ITapInterface)new OsXTapInterface(hostInterfaceName) : new LinuxTapInterface(hostInterfaceName, persistent);
+            else
+            {
+                result = new LinuxTapInterface(hostInterfaceName, persistent);
+            }
             emulation.HostMachine.AddHostMachineElement(result, name);
             return result;
         }
