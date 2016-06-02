06528fc8209a3553e213ee3af8903f8e7038797f
Mateusz Holenko <mholenko@antmicro.com>
2016-06-02 15:44:13 +0200

    [#5533] Add `BreakpointType` enum.

---
+++ Emulator/Main/Peripherals/CPU/BreakpointType.cs
@@ -0,0 +1,18 @@
+﻿//
+// Copyright (c) Antmicro
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+namespace Emul8.Peripherals.CPU
+{
+    public enum BreakpointType
+    {
+        MemoryBreakpoint,
+        HardwareBreakpoint,
+        WriteWatchpoint,
+        ReadWatchpoint,
+        AccessWatchpoint
+    }
+}
+
