3bde7b7d0d072fa6e3c316ff34f5be477be6c2ef
Mateusz Holenko <mholenko@antmicro.com>
2016-06-06 09:36:14 +0200

    [#6153] Add `IInterestingElement` interface.

---
+++ Tools/Bootstrap/IInterestingElement.cs
@@ -0,0 +1,14 @@
+﻿//
+// Copyright (c) Antmicro
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+namespace Emul8.Bootstrap
+{
+    public interface IInterestingElement
+    {
+        string Path { get; }
+    }
+}
+
