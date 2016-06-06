59950f17eb4f3e56064ce2cb49e05110c35fdbb6
Mateusz Holenko <mholenko@antmicro.com>
2016-06-06 09:42:59 +0200

    [#6153] Add new element to bootstrap: robot framework test suite.

---
+++ Tools/Bootstrap/Elements/RobotTestSuite.cs
@@ -0,0 +1,25 @@
+﻿//
+// Copyright (c) Antmicro
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+namespace Emul8.Bootstrap.Elements
+{
+    public class RobotTestSuite : IInterestingElement
+    {
+        public static bool TryCreate(string path, out IInterestingElement result)
+        {
+            result = new RobotTestSuite(path);
+            return true;
+        }
+
+        public RobotTestSuite(string path)
+        {
+            Path = System.IO.Path.GetFullPath(path);
+        }
+
+        public string Path { get; private set; }
+    }
+}
+
