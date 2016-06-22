0ed998718cb99796d5a82696a1a13ef7856ba685
Mateusz Holenko <mholenko@antmicro.com>
2016-06-22 10:51:10 +0200

    [#5533] Protect against writing to non-existing cpu register.

--- Emulator/Extensions/Utilities/GDB/Commands/WriteRegisterCommand.cs
+++ Emulator/Extensions/Utilities/GDB/Commands/WriteRegisterCommand.cs
@@ -4,6 +4,8 @@
 // This file is part of the Emul8 project.
 // Full license details are defined in the 'LICENSE' file.
 //
+using System.Linq;
+
 namespace Emul8.Utilities.GDB.Commands
 {
     internal class WriteRegisterCommand : Command
@@ -17,6 +19,11 @@ namespace Emul8.Utilities.GDB.Commands
             [Argument(Separator = '=')]int registerNumber, 
             [Argument(Encoding = ArgumentAttribute.ArgumentEncoding.HexBytesString)]uint value)
         {
+            if(!manager.Cpu.GetRegisters().Contains(registerNumber))
+            {
+                return PacketData.ErrorReply(0);
+            }
+
             manager.Cpu.SetRegisterUnsafe(registerNumber, value);
             return PacketData.Success;
         }
