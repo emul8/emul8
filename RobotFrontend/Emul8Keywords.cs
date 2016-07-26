a9791f7d582e5d3ca6624f639e528821612362b2
Mateusz Holenko <mholenko@antmicro.com>
2016-07-26 15:06:20 +0200

    [#6153] Use `CommandInteractionEater` instead of `MemoryCommandInteraction`.

--- RobotFrontend/Emul8Keywords.cs
+++ RobotFrontend/Emul8Keywords.cs
@@ -19,7 +19,7 @@ namespace Emul8.RobotFrontend
     {
         public Emul8Keywords()
         {
-            interaction = new MemoryCommandInteraction();
+            interaction = new CommandInteractionEater();
             monitor = new Monitor();
             monitor.Interaction = interaction;
         }
@@ -44,23 +44,26 @@ namespace Emul8.RobotFrontend
         public string ExecuteCommand(string[] commands)
         {
             var command = string.Join(" ", commands);
+            interaction.Clear();
             if(!monitor.Parse(command))
             {
-                throw new KeywordException("Could not execute command '{0}': {1}", command, interaction.Error);
+                throw new KeywordException("Could not execute command '{0}': {1}", command, interaction.GetError());
             }
 
-            return interaction.Output;
+            return interaction.GetContents();
         }
 
         [RobotFrameworkKeyword]
         public string ExecuteScript(string path)
         {
+            interaction.Clear();
+            
             if(!monitor.TryExecuteScript(path))
             {
-                throw new KeywordException("Could not execute script: {0}", interaction.Error);
+                throw new KeywordException("Could not execute script: {0}", interaction.GetError());
             }
 
-            return interaction.Output;
+            return interaction.GetContents();
         }
 
         [RobotFrameworkKeyword]
@@ -103,70 +106,7 @@ namespace Emul8.RobotFrontend
 
         private TerminalTester tester;
         private readonly Monitor monitor;
-        private readonly MemoryCommandInteraction interaction;
-
-        private class MemoryCommandInteraction : ICommandInteraction
-        {
-            public MemoryCommandInteraction()
-            {
-                output = new StringBuilder();
-                error = new StringBuilder();
-            }
-
-            public string Output 
-            { 
-                get
-                {
-                    lock(output)
-                    {
-                        var result = output.ToString();
-                        output.Clear();
-                        return result;
-                    }
-                }
-            }
-
-            public string Error
-            {
-                get
-                {
-                    lock(error)
-                    {
-                        var result = error.ToString();
-                        error.Clear();
-                        return result;
-                    }
-                }
-            }
-
-            public string CommandToExecute { get; set; }
-
-            public bool QuitEnvironment { get; set; }
-
-            public string ReadLine()
-            {
-                throw new InvalidOperationException("Reading from memory command interaction is impossible");
-            }
-
-            public void Write(char c, ConsoleColor? color = default(ConsoleColor?))
-            {
-                lock(output)
-                {
-                    output.Append(c);
-                }
-            }
-
-            public void WriteError(string errorMessage)
-            {
-                lock(error)
-                {
-                    error.Append(errorMessage);
-                }
-            }
-
-            private readonly StringBuilder output;
-            private readonly StringBuilder error;
-        }
+        private readonly CommandInteractionEater interaction;
     }
 }
 
