c42fade4d0ed32ff31d42ec23b3215d05e897b17
Mateusz Holenko <mholenko@antmicro.com>
2016-07-26 12:42:33 +0200

    [#6153] Move `IInterestingElement` to `Elements` folder.

--- Tools/Bootstrap/Elements/IInterestingElement.cs
+++ Tools/Bootstrap/Elements/IInterestingElement.cs
@@ -4,7 +4,7 @@
 // This file is part of the Emul8 project.
 // Full license details are defined in the 'LICENSE' file.
 //
-namespace Emul8.Bootstrap
+namespace Emul8.Bootstrap.Elements
 {
     public interface IInterestingElement
     {
