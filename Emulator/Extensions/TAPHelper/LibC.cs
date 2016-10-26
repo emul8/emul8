ed9f865969d516f00a109808897e3500d682e3fb
Mateusz Hołenko <mholenko@antmicro.com>
2016-10-26 15:14:16 +0200

    [#3232] Compile LibC on Linux only.

--- Emulator/Extensions/TAPHelper/LibC.cs
+++ Emulator/Extensions/TAPHelper/LibC.cs
@@ -5,6 +5,7 @@
 // This file is part of the Emul8 project.
 // Full license details are defined in the 'LICENSE' file.
 //
+#if EMUL8_PLATFORM_LINUX
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -174,4 +175,4 @@ namespace Emul8.TAPHelper
         #endregion
     }
 }
-
+#endif
