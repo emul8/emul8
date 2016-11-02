2956ff15467f7bd7e10994d71ffc8a049b45460b
Mateusz Hołenko <mholenko@antmicro.com>
2016-11-02 16:27:08 +0100

    [#3232] Do not compile `PtyUnixStream` on Windows.

--- Emulator/Main/Utilities/PtyUnixStream.cs
+++ Emulator/Main/Utilities/PtyUnixStream.cs
@@ -5,6 +5,7 @@
 // This file is part of the Emul8 project.
 // Full license details are defined in the 'LICENSE' file.
 //
+#if !EMUL8_PLATFROM_WINDOWS
 using System;
 using Mono.Unix.Native;
 using Mono.Unix;
@@ -226,4 +227,4 @@ namespace Emul8.Utilities
         private bool disposed;
     }
 }
-
+#endif
