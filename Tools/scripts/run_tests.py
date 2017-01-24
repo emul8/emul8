591f559c8239b6138851caece0b8afb87fb78d8b
Mateusz Hołenko <mholenko@antmicro.com>
2017-01-24 14:19:57 +0100

    [#7344] Rework run_tests script.

--- Tools/scripts/run_tests.py
+++ Tools/scripts/run_tests.py
@@ -1,49 +1,162 @@
 #!/usr/bin/python
+# pylint: disable=C0301,C0103,C0111
 from __future__ import print_function
 import os
+import re
 import sys
-import getopt
 import argparse
 import subprocess
-import signal
+import robot
+import fnmatch
 
 this_path = os.path.abspath(os.path.dirname(__file__))
-nunit_path = os.path.join(this_path, './../../External/Tools/nunit-console.exe')
-bin_directory = os.path.join(this_path, 'tests')
+results_directory = os.path.join(this_path, 'tests')
 
-def build_project(path):
-    """
-Tries to build a project at given path. Because of a bug in xbuild which sometimes crashes after build we retry it, up to 5 times, if received code is 134 (abort).
-"""
-    print("\t Building {0}".format(path))
-    for i in range(5):
-        ret_code = subprocess.call(['xbuild', '/p:OutputPath={0}'.format(bin_directory), '/nologo', '/verbosity:quiet', '/p:OutputDir=tests_output', '/p:Configuration={0}'.format(configuration), path])
-        if ret_code == 0:
-            return
-        elif ret_code != 134:
-            break
-    print("Building project `{}` failed with error code: {}".format(path, ret_code))
-    sys.exit(ret_code)
+class TestSuite(object):
+    @staticmethod
+    def get_type(path):
+        if path.endswith('csproj'):
+            return 'nunit'
+        if path.endswith('robot'):
+            return 'robot'
+        return None
 
-
-def ignore_sig():
-    signal.signal(signal.SIGTSTP, signal.SIG_IGN)
-
-def report_signal_handler(singnum, frame):
-    print("Test iteration {0}, currently on project `{1}`.".format(counter, project))
-
-
-def run(args):
-    process = subprocess.Popen(args, cwd=bin_directory, bufsize = 1, preexec_fn = ignore_sig, stdout =
-                subprocess.PIPE, stderr = subprocess.STDOUT)
-    while True:
-        line = process.stdout.readline()
-        ret = process.poll()
-        if ret is not None:
-            return ret == 0
+    @staticmethod
+    def create(path):
+        if TestSuite.get_type(path) == 'nunit':
+            return NUnitTestSuite(path)
+        elif TestSuite.get_type(path) == 'robot':
+            return RobotTestSuite(path)
         else:
+            raise Exception('Unknown test suite type: {}'.format(path))
+
+    def __init__(self, path):
+        self.path = path
+
+class NUnitTestSuite(TestSuite):
+    nunit_path = os.path.join(this_path, './../../External/Tools/nunit-console.exe')
+
+    def __init__(self, path):
+        super(NUnitTestSuite, self).__init__(path)
+
+    def prepare(self):
+        print("Building {0}".format(self.path))
+        result = subprocess.call(['xbuild', '/p:OutputPath={0}'.format(results_directory), '/nologo', '/verbosity:quiet', '/p:OutputDir=tests_output', '/p:Configuration={0}'.format(configuration), self.path])
+        if result != 0:
+            print("Building project `{}` failed with error code: {}".format(self.path, result))
+        return result
+
+    def run(self, fixture=None):
+        print('Running ' + self.path)
+        # copying nunit console binaries seems to be necessary in order to use -domain:None switch; otherwise it is not needed
+        copied_nunit_path = os.path.join(results_directory, 'nunit-console.exe')
+        if not os.path.isfile(copied_nunit_path):
+            subprocess.call(['bash', '-c', 'cp -r {0}/* {1}'.format(os.path.dirname(NUnitTestSuite.nunit_path), results_directory)])
+        args = ['mono', copied_nunit_path, '-domain:None', '-noshadow', '-nologo', '-labels', os.path.split(self.path)[1].replace("csproj", "dll")]
+        if options.port is not None:
+            if options.suspend:
+                print('Waiting for a debugger at port: {}'.format(options.port))
+            args.insert(1, '--debug')
+            args.insert(2, '--debugger-agent=transport=dt_socket,server=y,suspend={0},address=127.0.0.1:{1}'.format('y' if options.suspend else 'n', options.port))
+        elif options.debug_mode:
+            args.insert(1, '--debug')
+        if fixture:
+            args.append('-run:' + fixture)
+
+        process = subprocess.Popen(args, cwd=results_directory, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        while True:
+            line = process.stdout.readline()
+            ret = process.poll()
+            if ret is not None:
+                return ret == 0
             if line and not line.isspace() and 'GLib-' not in line:
                 output.write(line)
+
+    def cleanup(self):
+        pass
+
+class RobotTestSuite(TestSuite):
+    instances_count = 0
+    emul8_robot_frontend_process = None
+    hotspot_action = ['None', 'Pause']
+    log_files = []
+
+    def __init__(self, path):
+        super(RobotTestSuite, self).__init__(path)
+
+    def prepare(self):
+        if RobotTestSuite.instances_count > 0:
+            return
+        RobotTestSuite.instances_count += 1
+
+        emul8_robot_frontend_binary_folder = os.path.join(this_path, '../../output/{0}'.format(configuration))
+        emul8_robot_frontend_binary = os.path.join(emul8_robot_frontend_binary_folder, 'RobotFrontend.exe')
+
+        if not os.path.isfile(emul8_robot_frontend_binary):
+            print("Robot frontend binary not found! Did you forget to bootstrap and build the Emul8?")
+            sys.exit(1)
+
+        RobotTestSuite.emul8_robot_frontend_process = subprocess.Popen(['mono', emul8_robot_frontend_binary, '9999'], cwd=emul8_robot_frontend_binary_folder, bufsize=1)
+
+    def run(self, fixture=None):
+        result = True
+
+        tests_with_hotspots = {}
+        for hotspot in RobotTestSuite.hotspot_action:
+            tests_with_hotspots[hotspot] = []
+        tests_without_hotspots = []
+        _suite = robot.parsing.model.TestData(source=self.path)
+        for test in _suite.testcase_table.tests:
+            found_anything = False
+            for step in test.steps:
+                if step.name == 'Hot Spot':
+                    for hotspot in RobotTestSuite.hotspot_action:
+                        tests_with_hotspots[hotspot].append('{0}.{1}'.format(RobotTestSuite._create_suite_name(_suite.name, hotspot), test.name))
+                    found_anything = True
+                    break
+            if not found_anything:
+                tests_without_hotspots.append('{0}.{1}'.format(_suite.name, test.name))
+
+        if len(tests_without_hotspots) > 0:
+            result = result and self._run_inner(fixture, None, tests_without_hotspots)
+        for hotspot in RobotTestSuite.hotspot_action:
+            if len(tests_with_hotspots[hotspot]) > 0:
+                result = result and self._run_inner(fixture, hotspot, tests_with_hotspots[hotspot])
+
+        return result
+
+    def cleanup(self):
+        RobotTestSuite.instances_count -= 1
+        if RobotTestSuite.instances_count == 0:
+            if RobotTestSuite.emul8_robot_frontend_process:
+                os.kill(RobotTestSuite.emul8_robot_frontend_process.pid, 15)
+                RobotTestSuite.emul8_robot_frontend_process.wait()
+            if len(RobotTestSuite.log_files) > 0:
+                print("Aggregating all robot results")
+                robot.rebot(*RobotTestSuite.log_files, processemptysuite=True, name='Emul8 Suite', outputdir=results_directory, output='output.xml')
+
+    @staticmethod
+    def _create_suite_name(test_name, hotspot):
+        return test_name + (' [HotSpot action: {0}]'.format(hotspot) if hotspot else '')
+
+    def _run_inner(self, fixture, hotspot, test_cases):
+        file_name = os.path.splitext(os.path.basename(self.path))[0]
+        log_file = os.path.join(results_directory, '{0}{1}.xml'.format(file_name, '_' + hotspot if hotspot else ''))
+        suite_name = RobotTestSuite._create_suite_name(file_name, hotspot)
+        metadata = 'HotSpot_Action:{0}'.format(hotspot if hotspot else '-')
+
+        variables = ['SKIP_RUNNING_SERVER:True']
+        if hotspot:
+            variables.append('HOTSPOT_ACTION:' + hotspot)
+        if options.debug_mode:
+            variables.extend('CONFIGURATION:Debug')
+        if fixture:
+            test_cases = fnmatch.filter(test_cases, fixture)
+            if len(test_cases) == 0:
+                return True
+
+        RobotTestSuite.log_files.append(log_file)
+        return robot.run(self.path, runemptysuite=True, output=log_file, log=None, report=None, metadata=metadata, name=suite_name, variable=variables, test=test_cases) == 0
 
 # parsing cmd-line arguments
 parser = argparse.ArgumentParser()
@@ -56,7 +169,8 @@ parser.add_argument("-b", "--buildbot", dest="buildbot",    action="store_true",
 parser.add_argument("-t", "--tests",    dest="tests_file",  action="store",       default=None,  help="Path to a file with a list of assemblies with tests to run.")
 parser.add_argument("-p", "--port",     dest="port",        action="store",       default=None,  help="Debug port.")
 parser.add_argument("-s", "--suspend",  dest="suspend",     action="store_true",  default=False, help="Suspend test waiting for a debugger.")
-options  = parser.parse_args()
+parser.add_argument("-T", "--type",     dest="test_type",   action="store",       default="all", help="Type of test to execute: nunit, robot or all (default)")
+options = parser.parse_args()
 
 if options.buildbot:
     print("Preparing Environment")
@@ -88,77 +202,32 @@ if not options.output is None:
     try:
         output = open(options.output)
     except Exception as e:
-        print("Failed opening output file. Falling back to STDOUT.")
+        print("Failed to open output file. Falling back to STDOUT.")
 
-#register signal handlers
-signal.signal(signal.SIGUSR1, report_signal_handler)
-signal.signal(signal.SIGTSTP, report_signal_handler)
+tests_suites = [TestSuite.create(path) for path in options.tests if options.test_type == "all" or TestSuite.get_type(path) == options.test_type]
 
-print("Building projects.")
-for project in options.tests:
-    if project.endswith('csproj'):
-        build_project(project)
+print("Preparing suites")
+for suite in tests_suites:
+    suite.prepare()
 
-print("Starting tests. Use STOP signal (default: Ctrl+Z) to check progress.")
-
-#main loop
+print("Starting suites")
 tests_failed = False
 counter = 0
 while options.repeat_count == 0 or counter < options.repeat_count:
     counter += 1
-
-    robot_tests = []
-    for project in options.tests:
-        if project.endswith('csproj'):
-            filename = os.path.split(project)[1]
-            copied_nunit_path = os.path.join(bin_directory, 'nunit-console.exe')
-            if not os.path.isfile(copied_nunit_path):
-                subprocess.call(['bash', '-c', 'cp -r ' + os.path.dirname(nunit_path) + '/* ' + bin_directory])
-
-            args = ['mono', copied_nunit_path, '-noshadow', '-nologo', '-labels', '-domain:None', filename.replace("csproj", "dll")]
-            if options.port is not None:
-                if options.suspend:
-                    print('Waiting for a debugger at port: {}'.format(options.port))
-                args.insert(1, '--debug')
-                args.insert(2, '--debugger-agent=transport=dt_socket,server=y,suspend={0},address=127.0.0.1:{1}'.format('y' if options.suspend else 'n', options.port))
-            elif options.debug_mode:
-                args.insert(1, '--debug')
-            if options.fixture:
-                args.append('-run:' + options.fixture)
-        elif project.endswith('robot'):
-            robot_tests.append(project)
-            continue
-
-        if not run(args):
+    for suite in tests_suites:
+        if not suite.run(options.fixture):
             tests_failed = True
 
-    if any(robot_tests):
-
-        emul8_robot_frontend_binary_folder = os.path.join(this_path, '../../output/{0}'.format(configuration))
-        emul8_robot_frontend_binary = os.path.join(emul8_robot_frontend_binary_folder, 'RobotFrontend.exe')
-
-        if not os.path.isfile(emul8_robot_frontend_binary):
-            print("Robot frontend binary not found! Have you forgot to bootstrap and build the Emul8?")
-            sys.exit(1)
-
-        emul8_robot_frontend_process = subprocess.Popen(['mono', emul8_robot_frontend_binary, '9999'], cwd=emul8_robot_frontend_binary_folder, bufsize=1)
-
-        args = ['robot', '-N', 'Emul8_Suite', '-C', 'on', '-v', 'SKIP_RUNNING_SERVER:True']
-        if(options.debug_mode):
-            args.append('-v')
-            args.append('CONFIGURATION:Debug')
-        args.extend(robot_tests)
-
-        if not run(args):
-            tests_failed = True
-
-        os.kill(emul8_robot_frontend_process.pid, 15)
-        emul8_robot_frontend_process.wait()
+print("Cleaning up suites")
+for suite in tests_suites:
+    suite.cleanup()
 
 output.flush()
-if not output is sys.stdout:
+if output is not sys.stdout:
     output.close()
 
 if tests_failed:
-    print("Some tests failed! See logs for details!")
+    print("Some tests failed :( See logs for details!")
     sys.exit(1)
+print("Tests finished sucessfully :)")
