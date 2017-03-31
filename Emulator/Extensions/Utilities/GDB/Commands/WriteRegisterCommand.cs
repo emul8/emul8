9155de4445d9997326382db46a9dfc4b386d4250
Mateusz Hołenko <mholenko@antmicro.com>
2017-03-31 09:36:48 +0200

    [FIX] Fix setting cpu register value using gdb.

--- Emulator/Extensions/Utilities/GDB/Commands/WriteRegisterCommand.cs
+++ Emulator/Extensions/Utilities/GDB/Commands/WriteRegisterCommand.cs
@@ -5,6 +5,7 @@
 // Full license details are defined in the 'LICENSE' file.
 //
 using System.Linq;
+using System;
 
 namespace Emul8.Utilities.GDB.Commands
 {
@@ -16,15 +17,15 @@ namespace Emul8.Utilities.GDB.Commands
 
         [Execute("P")]
         public PacketData Execute(
-            [Argument(Separator = '=')]int registerNumber, 
-            [Argument(Encoding = ArgumentAttribute.ArgumentEncoding.HexBytesString)]uint value)
+            [Argument(Encoding = ArgumentAttribute.ArgumentEncoding.HexNumber, Separator = '=')]int registerNumber,
+            [Argument(Encoding = ArgumentAttribute.ArgumentEncoding.HexBytesString)]byte[] value)
         {
             if(!manager.Cpu.GetRegisters().Contains(registerNumber))
             {
                 return PacketData.ErrorReply(0);
             }
 
-            manager.Cpu.SetRegisterUnsafe(registerNumber, value);
+            manager.Cpu.SetRegisterUnsafe(registerNumber, BitConverter.ToUInt32(value, 0));
             return PacketData.Success;
         }
     }
