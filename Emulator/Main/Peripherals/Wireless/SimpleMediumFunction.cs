33fe5eec318e3ba6a912671d52769b5f3f2a2dc3
Michał Szprejda <mszprejda@antmicro.com>
2017-06-05 12:57:54 +0200

    [#8262] Cleanup of radio medium API.

--- Emulator/Main/Peripherals/Wireless/SimpleMediumFunction.cs
+++ Emulator/Main/Peripherals/Wireless/SimpleMediumFunction.cs
@@ -8,6 +8,14 @@ using System;
 
 namespace Emul8.Peripherals.Wireless
 {
+    public static class SimpleMediumExtension
+    {
+        public static void SetSimpleWirelessFunction(this WirelessMedium wirelessMedium)
+        {
+            wirelessMedium.SetMediumFunction(SimpleMediumFunction.Instance);
+        }
+    }
+
     public sealed class SimpleMediumFunction : IMediumFunction
     {
         public static SimpleMediumFunction Instance { get; private set; }
@@ -19,12 +27,22 @@ namespace Emul8.Peripherals.Wireless
 
         private SimpleMediumFunction()
         {
-            
         }
 
         public bool CanReach(Position from, Position to)
         {
+            // always can reach destination - that's why we call it `simple`
             return true;
         }
+
+        public bool CanTransmit(Position from)
+        {
+            // always can transmit - that's why we call it `simple`
+            return true;
+        }
+
+        public string FunctionName { get { return Name; } }
+
+        private const string Name = "simple_medium_function";
     }
 }
