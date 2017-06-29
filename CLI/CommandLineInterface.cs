123b5702aa41bbcbb34f60e2750ba5c6bcd9056d
Mateusz Hołenko <mholenko@antmicro.com>
2017-06-29 12:27:32 +0200

    [#8323] Refactor `CommandLineInterface`.

--- CLI/CommandLineInterface.cs
+++ CLI/CommandLineInterface.cs
@@ -10,7 +10,6 @@ using Emul8.Logging;
 using Emul8.Core;
 using Emul8.UserInterface;
 using Emul8.Backends.Terminals;
-using System.Threading;
 using AntShell;
 using AntShell.Terminal;
 using Emul8.Exceptions;
@@ -24,15 +23,8 @@ namespace Emul8.CLI
 {
     public static class CommandLineInterface
     {
-        public static void Run(string[] args)
+        public static void Run(Options options, Action<ObjectCreator.Context> beforeRun = null)
         {
-            var options = new Options();
-            var optionsParser = new OptionsParser();
-            if(!optionsParser.Parse(options, args))
-            {
-                return;
-            }
-
             XwtProvider xwt = null;
             try
             {
@@ -54,64 +46,31 @@ namespace Emul8.CLI
                     EmulationManager.Instance.ProgressMonitor.Handler = new CLIProgressMonitor();
                     AppDomain.CurrentDomain.UnhandledException += (sender, e) => CrashHandler.HandleCrash((Exception)e.ExceptionObject);
 
-                    Shell shell = null;
-                    if(options.Port > 0)
+                    if(options.Port == -1)
                     {
-                        var io = new IOProvider(new SocketIOSource(options.Port));
-                        shell = ShellProvider.GenerateShell(io, monitor, true, false);
-                    }
-                    else
-                    {
-                        if(xwt == null)
-                        {
-                            Logger.Log(LogLevel.Error, "XWT provider is needed to run Emul8 in selected mode.");
-                            return;
-                        }
-                        EmulationManager.Instance.CurrentEmulation.BackendManager.SetPreferredAnalyzer(typeof(UARTBackend),  typeof(UARTWindowBackendAnalyzer));
+                        EmulationManager.Instance.CurrentEmulation.BackendManager.SetPreferredAnalyzer(typeof(UARTBackend), typeof(UARTWindowBackendAnalyzer));
                         EmulationManager.Instance.EmulationChanged += () =>
                         {
                             EmulationManager.Instance.CurrentEmulation.BackendManager.SetPreferredAnalyzer(typeof(UARTBackend), typeof(UARTWindowBackendAnalyzer));
                         };
-
-                        var terminal = new UARTWindowBackendAnalyzer();
-                        shell = ShellProvider.GenerateShell(terminal.IO, monitor);
-                        monitor.Quitted += shell.Stop;
-
-                        try
-                        {
-                            terminal.Show();
-                        }
-                        catch(InvalidOperationException ex)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Error.WriteLine(ex.Message);
-                            Emulator.Exit();
-                        }
                     }
-                    shell.Quitted += Emulator.Exit;
 
-                    monitor.Interaction = shell.Writer;
-                    monitor.MachineChanged += emu => shell.SetPrompt(emu != null ? new Prompt(string.Format("({0}) ", emu), ConsoleColor.DarkYellow) : null);
-
-                    if(options.Execute != null)
+                    var shell = PrepareShell(options, monitor);
+                    new System.Threading.Thread(x => shell.Start(true))
                     {
-                        shell.Started += s => s.InjectInput(string.Format("{0}\n", options.Execute));
-                    }
-                    else if(!string.IsNullOrEmpty(options.ScriptPath))
-                    {
-                        shell.Started += s => s.InjectInput(string.Format("i {0}{1}\n", Path.IsPathRooted(options.ScriptPath) ? "@" : "$CWD/", options.ScriptPath));
-                    }
-
-                    new Thread(x => shell.Start(true)) 
-                    { 
-                        IsBackground = true, 
-                        Name = "Shell thread" 
+                        IsBackground = true,
+                        Name = "Shell thread"
                     }.Start();
 
                     Emulator.BeforeExit += () =>
                     {
                         Emulator.DisposeAll();
                     };
+
+                    if(beforeRun != null)
+                    {
+                        beforeRun(context);
+                    }
 
                     Emulator.WaitForExit();
                 }
@@ -123,6 +82,48 @@ namespace Emul8.CLI
                     xwt.Dispose();
                 }
             }
+        }
+
+        private static Shell PrepareShell(Options options, Monitor monitor)
+        {
+            Shell shell = null;
+            if(options.Port >= 0)
+            {
+                var io = new IOProvider(new SocketIOSource(options.Port));
+                shell = ShellProvider.GenerateShell(io, monitor, true, false);
+            }
+            else
+            {
+                var terminal = new UARTWindowBackendAnalyzer();
+                shell = ShellProvider.GenerateShell(terminal.IO, monitor);
+                monitor.Quitted += shell.Stop;
+
+                try
+                {
+                    terminal.Show();
+                }
+                catch(InvalidOperationException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Error.WriteLine(ex.Message);
+                    Emulator.Exit();
+                }
+            }
+            shell.Quitted += Emulator.Exit;
+
+            monitor.Interaction = shell.Writer;
+            monitor.MachineChanged += emu => shell.SetPrompt(emu != null ? new Prompt(string.Format("({0}) ", emu), ConsoleColor.DarkYellow) : null);
+
+            if(options.Execute != null)
+            {
+                shell.Started += s => s.InjectInput(string.Format("{0}\n", options.Execute));
+            }
+            else if(!string.IsNullOrEmpty(options.ScriptPath))
+            {
+                shell.Started += s => s.InjectInput(string.Format("i {0}{1}\n", Path.IsPathRooted(options.ScriptPath) ? "@" : "$CWD/", options.ScriptPath));
+            }
+
+            return shell;
         }
     }
 }
--- CLI/Program.cs
+++ CLI/Program.cs
@@ -5,7 +5,11 @@
 // This file is part of the Emul8 project.
 // Full license details are defined in the 'LICENSE' file.
 //
+using System;
+using System.IO;
 using System.Threading;
+using Antmicro.OptionsParser;
+using Emul8.Utilities;
 using Emul8.Logging;
 
 namespace Emul8.CLI
@@ -15,14 +19,35 @@ namespace Emul8.CLI
         public static void Main(string[] args)
         {
             ConsoleBackend.Instance.WindowTitle = "Emul8";
+            ConfigureEnvironment();
 
             var thread = new Thread(() =>
             {
-                CommandLineInterface.Run(args);
+                var options = new Options();
+                var optionsParser = new OptionsParser();
+                if(!optionsParser.Parse(options, args))
+                {
+                    return;
+                }
+
+                CommandLineInterface.Run(options);
                 Emulator.FinishExecutionAsMainThread();
             });
             thread.Start();
             Emulator.ExecuteAsMainThread();
+        }
+
+        private static void ConfigureEnvironment()
+        {
+            string emul8Dir;
+            if(Misc.TryGetEmul8Directory(out emul8Dir))
+            {
+                var localConfig = Path.Combine(emul8Dir, "emul8.config");
+                if(File.Exists(localConfig))
+                {
+                    ConfigurationManager.Initialize(localConfig);
+                }
+            }
         }
     }
 }
