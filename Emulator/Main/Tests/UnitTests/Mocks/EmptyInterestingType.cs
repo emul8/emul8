19b4ddcc0de05ac9f1065776a1946929d89ca2cb
Michał Szprejda <mszprejda@antmicro.com>
2017-07-20 10:56:59 +0200

    [#8543] Add mocks for registration and cast testing.

---
+++ Emulator/Main/Tests/UnitTests/Mocks/EmptyInterestingType.cs
@@ -0,0 +1,26 @@
+﻿//
+// Copyright (c) Antmicro
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+using Emul8.Core.Structure;
+using Emul8.Peripherals.CPU;
+using Emul8.Utilities;
+
+namespace Emul8.UnitTests.Mocks
+{
+    public class EmptyInterestingType : IPeripheralRegister<ICPU, NullRegistrationPoint>
+    {
+        // note: Register and Unregister methods are empty, because the purpose of this type is to test
+        // casting of types (that is why this type does not implement IPeripheral) and they will not be used
+
+        public void Register(ICPU peripheral, NullRegistrationPoint registrationPoint)
+        {
+        }
+
+        public void Unregister(ICPU peripheral)
+        {
+        }
+    }
+}
---
+++ Emulator/Main/Tests/UnitTests/Mocks/MockRegister.cs
@@ -0,0 +1,66 @@
+﻿//
+// Copyright (c) Antmicro
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+using System;
+using Emul8.Core;
+using Emul8.Core.Structure;
+using Emul8.Peripherals;
+using Emul8.Peripherals.Bus;
+using Emul8.Peripherals.CPU;
+
+namespace Emul8.UnitTests.Mocks
+{
+    public class MockRegister : IPeripheralRegister<ICPU, NullRegistrationPoint>, IDoubleWordPeripheral, IKnownSize
+    {
+        public MockRegister(Machine machine)
+        {
+            this.machine = machine;
+        }
+
+        public void Register(ICPU peripheral, NullRegistrationPoint registrationPoint)
+        {
+            if(isRegistered)
+            {
+                throw new ArgumentException("Child is already registered.");
+            }
+            else
+            {
+                isRegistered = true;
+                machine.RegisterAsAChildOf(this, peripheral, registrationPoint);
+            }
+        }
+
+        public void Unregister(ICPU peripheral)
+        {
+            isRegistered = false;
+            machine.UnregisterAsAChildOf(this, peripheral);
+        }
+
+        public void Reset()
+        {
+        }
+
+        public uint ReadDoubleWord(long offset)
+        {
+            return 0;
+        }
+
+        public void WriteDoubleWord(long offset, uint value)
+        {
+        }
+
+        public long Size
+        {
+            get 
+            {
+                return 0x4;
+            }
+        }
+
+        private bool isRegistered;
+        private Machine machine;
+    }
+}
