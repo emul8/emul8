0a5d7e65c7a67ce14ae629c8e135305a6bfc6bf0
Mateusz Hołenko <mholenko@antmicro.com>
2017-08-07 14:46:53 +0200

    [FIX] Make links to llvm disassembler binaries relative.

--- Tools/scripts/fetch_libraries.sh
+++ Tools/scripts/fetch_libraries.sh
@@ -5,13 +5,13 @@ set -u
 
 REMOTE=https://github.com/antmicro/emul8-libraries.git
 ROOT_PATH="`dirname \"\`realpath "$0"\`\"`"
-DIR="$ROOT_PATH/../../External/emul8-libraries"
 GUARD=`realpath --relative-to="$PWD" "$ROOT_PATH/../../External/.emul8_libs_fetched"`
+EMUL8_LIBRARIES_DIR="$ROOT_PATH/../../External/emul8-libraries"
 
 if [ -e "$GUARD" ]
 then
     top_ref=`git ls-remote -h $REMOTE master | cut -f1`
-    pushd "$DIR" >/dev/null
+    pushd "$EMUL8_LIBRARIES_DIR" >/dev/null
     cur_ref=`git rev-parse HEAD`
     master_ref=`git rev-parse master`
     if [ $master_ref != $cur_ref ]
@@ -28,14 +28,20 @@ then
     echo "Required Emul8 libraries are available in a new version. The libraries will be redownloaded..."
 fi
 
-rm -rf "$DIR" "$ROOT_PATH"/../../External/{Lib,Tools} "$ROOT_PATH/../../Emulator/LLVMDisassembler/Resources/"
+rm -rf "$EMUL8_LIBRARIES_DIR" "$ROOT_PATH"/../../External/{Lib,Tools} "$ROOT_PATH/../../Emulator/LLVMDisassembler/Resources/"
+
+git clone $REMOTE "`realpath --relative-to="$PWD" "$EMUL8_LIBRARIES_DIR"`"
+
+ln -s "$EMUL8_LIBRARIES_DIR/Lib" "$ROOT_PATH/../../External/Lib"
+ln -s "$EMUL8_LIBRARIES_DIR/Tools" "$ROOT_PATH/../../External/Tools"
 
 mkdir -p "$ROOT_PATH/../../Emulator/LLVMDisassembler/Resources/"
-git clone $REMOTE "$DIR"
-ln -s "$DIR/Lib" "$ROOT_PATH/../../External/Lib"
-ln -s "$DIR/Tools" "$ROOT_PATH/../../External/Tools"
 pushd "$ROOT_PATH/../../Emulator/LLVMDisassembler/Resources" >/dev/null
-ln -s "$DIR/llvm/"* .
+LLVM_BINARIES_DIR=`realpath --relative-to="$PWD" "$EMUL8_LIBRARIES_DIR/llvm"`
+for f in `ls "$LLVM_BINARIES_DIR"`
+do
+    ln -s "$LLVM_BINARIES_DIR/$f"
+done
 popd >/dev/null
 
 touch "$GUARD"
