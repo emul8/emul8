960fbcb56e7a6d725158824297314157c3dca8e0
Michał Szprejda <mszprejda@antmicro.com>
2017-10-05 15:42:37 +0200

    [#8789] Make directory separator consistent in Monitor.

--- Emulator/Extensions/UserInterface/Monitor.cs
+++ Emulator/Extensions/UserInterface/Monitor.cs
@@ -783,7 +783,19 @@ namespace Emul8.UserInterface
                                      .Select(x => allButLast + "@" + StripPrefix(x, prefix).Replace(" ", @"\ "));
                 var dirs = Directory.GetDirectories(directoryPath, lastElement + '*', SearchOption.TopDirectoryOnly)
                                     .Select(x => allButLast + "@" + (StripPrefix(x, prefix) + '/').Replace(" ", @"\ "));
-                return files.Concat(dirs);
+
+                var result = new List<string>();
+                //We change "\" characters to "/", unless they were followed by the space character, in which case they treated as escape char.
+                foreach(var file in files.Concat(dirs))
+                {
+                    var sanitizedFile = file.Replace("\\", "/");
+                    if(sanitizedFile.Contains("/ "))
+                    {
+                        sanitizedFile = sanitizedFile.Replace("/ ", "\\ ");
+                    }
+                    result.Add(sanitizedFile);
+                }
+                return result;
             }
             catch(UnauthorizedAccessException)
             {
