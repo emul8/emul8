b14bd1b9014de24c4ac57ce92491995794287401
Michał Szprejda <mszprejda@antmicro.com>
2017-10-10 15:22:21 +0200

    [#7494] Added dummy SPI loopback device.

---
+++ Emulator/Main/Peripherals/SPI/SPILoopback.cs
@@ -0,0 +1,25 @@
+﻿﻿//
+// Copyright (c) Antmicro
+//
+// This file is part of the Emul8 project.
+// Full license details are defined in the 'LICENSE' file.
+//
+namespace Emul8.Peripherals.SPI
+{
+    //This is a dummy device, emulating connection between MISO and MOSI lines.
+    public class SPILoopback : ISPIPeripheral
+    {
+        public void FinishTransmission()
+        {
+        }
+
+        public void Reset()
+        {
+        }
+
+        public byte Transmit(byte data)
+        {
+            return data;
+        }
+    }
+}
