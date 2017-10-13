5ae3ddbe7f6d6acf8d07c0ff4148ae2a2f2aa369
Michał Szprejda <mszprejda@antmicro.com>
2017-10-13 15:55:49 +0200

    [#8789] Extract method from fetch_libraries scripts to common.

--- Tools/scripts/fetch_libraries.sh
+++ Tools/scripts/fetch_libraries.sh
@@ -10,29 +10,9 @@ EMUL8_LIBRARIES_DIR="$CURRENT_PATH/../../External/emul8-libraries"
 
 source "$CURRENT_PATH/../common.sh"
 
-if [ -e "$GUARD" ]
-then
-    top_ref=`git ls-remote -h $REMOTE master | cut -f1`
-    pushd "$EMUL8_LIBRARIES_DIR" >/dev/null
-    cur_ref=`git rev-parse HEAD`
-    master_ref=`git rev-parse master`
-    if [ $master_ref != $cur_ref ]
-    then
-        echo "The Emul8 libraries repository is not on the local master branch. This situation should be handled manually."
-        exit
-    fi
-    popd >/dev/null
-    if [ $top_ref == $cur_ref ]
-    then
-        echo "Required Emul8 libraries already downloaded. To repeat the process remove $GUARD file."
-        exit
-    fi
-    echo "Required Emul8 libraries are available in a new version. The libraries will be redownloaded..."
-fi
+clone_if_necessary "Emul8" "$REMOTE" "$EMUL8_LIBRARIES_DIR" "$GUARD" "$PWD"
 
-rm -rf "$EMUL8_LIBRARIES_DIR" "$CURRENT_PATH"/../../External/{Lib,Tools} "$CURRENT_PATH/../../Emulator/LLVMDisassembler/Resources/"
-
-git clone $REMOTE "`realpath --relative-to="$PWD" "$EMUL8_LIBRARIES_DIR"`"
+rm -rf "$CURRENT_PATH"/../../External/{Lib,Tools} "$CURRENT_PATH/../../Emulator/LLVMDisassembler/Resources/"
 
 TOOLS_TARGET_DIR="$CURRENT_PATH/../../External/Tools"
 LIB_TARGET_DIR="$CURRENT_PATH/../../External/Lib"
